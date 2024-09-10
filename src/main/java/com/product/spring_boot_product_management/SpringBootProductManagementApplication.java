28c19e2086508bd75d831a445f8c6ef65106bf10
RajiAujla <rajwinderaujla061@gmail.com>
2024-09-09 23:37:51 -0400

    Project setup

---
+++ src/main/java/com/product/spring_boot_product_management/SpringBootProductManagementApplication.java
@@ -0,0 +1,13 @@
+package com.product.spring_boot_product_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProductManagementApplication.class, args);
+	}
+
+}
